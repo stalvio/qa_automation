5c283d52cba954d40a4c64d0b5badb04b3c5dde9
stalvio <stalvio.net@gmail.com>
2021-11-25 12:46:25 +0200

    fix failed UI tests

--- final-test-framework/src/test/java/ui/ua/prom/resources/TestDataProvider.java
+++ final-test-framework/src/test/java/ui/ua/prom/resources/TestDataProvider.java
@@ -8,7 +8,7 @@ public class TestDataProvider {
 
     @DataProvider(name = "search-field-request-data")
     public Object[] searchRequestDataProv() {
-        return new Object[] {"ламп", "футболк"};
+        return new Object[] {"adapter", "samsung"};
     }
 
     @DataProvider(name = "bottom-top-price-data")
@@ -21,7 +21,6 @@ public class TestDataProvider {
     @DataProvider(name = "search-request-items-to-buy-data")
     public Object[][] itemsToBuyDataProvider() {
         return new Object[][]{
-                {"ламп","Светодиодная кольцевая лампа кольцо для селфи фото с держателем для телефона RL-18 45 см(LED/Лед свет, Selfie)"},
                 {"ноут", "Блок питания для ноута HP 19.5V 4.62A 90W 4.8x1.7 мм + кабель питания (2091) KM"}
         };
     }
